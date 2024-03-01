7576c9d63d70d8ec786b38102b9abb0c8164ce09
Cameron Cardinal <cameriancardinal@gmail.com>
2024-03-01 11:55:56 +0100

    banner responsive

--- www/views/template.view.php
+++ www/views/template.view.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Centre de formation professionnelle Sainte Barbe</title>
     <link rel="stylesheet" href=".\assets\styles\style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="./public/fonts/tondu/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <header class="banner" st>
@@ -22,6 +24,18 @@
                         </a>
                     </h1>
                     <p class="blog-tagline">Métiers du bâtiment, de l'éco-construction, et de l'éco-rénovation</p>
+                </div>
+                <div class="recherche-header">
+                    <form role="search" method="get" class="search-form" action="<?= SITE_URL ?>">
+                        <label>
+                            <span class="screen-reader-text">Rechercher&nbsp;</span>
+                            <input type="search" class="search-field" placeholder="Recherche..." value name="s">
+                        </label>
+                        <button type="submit" class="search-submit">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                            <span class="sr-only">Rechercher</span>
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
